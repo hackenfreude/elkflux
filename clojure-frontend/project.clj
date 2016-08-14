766c53a4154a927d198335675e5c539887156cfb
hackenfreude <hackenfreude@users.noreply.github.com>
2016-08-14 08:38:02 -0700

    log4j console appender

--- clojure-frontend/project.clj
+++ clojure-frontend/project.clj
@@ -1,6 +1,8 @@
 (defproject clojure-frontend "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring.middleware.logger "0.5.0"]
+                 [clj-logging-config "1.9.12"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clojure-frontend.core/app})
--- clojure-frontend/src/clojure_frontend/core.clj
+++ clojure-frontend/src/clojure_frontend/core.clj
@@ -1,11 +1,29 @@
 (ns clojure-frontend.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.logger :as logger])
+  (:use
+    clj-logging-config.log4j
+    clojure.tools.logging))
+
+(defn handle-get []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>Hello World</h1>"})
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (handle-get))
   (route/not-found "Not Found"))
 
+(def init
+  (do
+    (set-loggers!
+      :root {:out :console :level :error}
+      "onelog.core" {:out :console :level :info})
+    (info "logging initialized")))
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (->
+    (wrap-defaults app-routes site-defaults)
+    (logger/wrap-with-logger)))
